266eb6be41a8c4cfe0cd95448132bc5099d80bbc
Anton Voloshin <vanamp@yandex.ru>
2022-03-16 19:29:13 +0300

    Начальная фиксация

---
+++ temp.py
@@ -0,0 +1,15 @@
+i: str = 'hello world'  
+
+i: <int> = 42 
+
+i: int = 'hello world' 
+
+i: int = 10 
+
+def is_it_lie(val: bool): ... 
+
+def how_mach(num: <float>): ... 
+
+def what_is_it: None(name: str='it'): ... 
+
+def who_am_i(name: str='hero'): None: ... 
